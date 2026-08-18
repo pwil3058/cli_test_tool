8725719e928457feb09198633597ae9916459688
Peter Williams <pwil3058@bigpond.net.au>
2026-08-18 14:33:00 +1000

    parser: finish and test

--- src/parse/parser_tests.rs
+++ src/parse/parser_tests.rs
@@ -9,13 +9,19 @@ fn test_command_parse() {
         action,
         CommandAction::SetEnvVar("PATH".to_string(), "/usr/bin:/bin".to_string())
     );
+
     assert!(action.parse_text("unset WHATEVER", "label").is_ok());
     assert_eq!(action, CommandAction::UnsetEnvVar("WHATEVER".to_string()));
+
+    assert!(action.parse_text("cd WHATEVER", "label").is_ok());
+    assert_eq!(action, CommandAction::ChangeDir("WHATEVER".to_string()));
+
     assert!(action.parse_text("ls", "label").is_ok());
     assert_eq!(
         action,
         CommandAction::RunProgram("ls".to_string(), vec![], None, None, None)
     );
+
     assert!(action.parse_text("echo hello world", "label").is_ok());
     assert_eq!(
         action,
@@ -27,5 +33,36 @@ fn test_command_parse() {
             None
         )
     );
-    assert!("\"target".starts_with('"'));
+
+    assert!(
+        action
+            .parse_text("echo hello world < something > else", "label")
+            .is_ok()
+    );
+    assert_eq!(
+        action,
+        CommandAction::RunProgram(
+            "echo".to_string(),
+            vec!["hello".to_string(), "world".to_string()],
+            Some("something".to_string()),
+            Some(("else".to_string(), true)),
+            None
+        )
+    );
+
+    assert!(
+        action
+            .parse_text("echo hello world < something >> else 2> error", "label")
+            .is_ok()
+    );
+    assert_eq!(
+        action,
+        CommandAction::RunProgram(
+            "echo".to_string(),
+            vec!["hello".to_string(), "world".to_string()],
+            Some("something".to_string()),
+            Some(("else".to_string(), false)),
+            Some(("error".to_string(), true))
+        )
+    );
 }
